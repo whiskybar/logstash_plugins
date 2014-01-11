0af65c4908c8480252e2d6622482dc1da9d5eb4d
Jiri Barton <jbar@hosting4u.cz>
2014-01-11 13:48:20 +0100

    added converting values

--- logstash/outputs/tcp_custom.rb
+++ logstash/outputs/tcp_custom.rb
@@ -1,12 +1,24 @@
+require "scanf"
 require "logstash/outputs/tcp"
 require "logstash/namespace"
 
 class LogStash::Outputs::TcpCustom < LogStash::Outputs::Tcp
+  # Write events over a TCP socket.
+  #
+  # In addition to the built-in Tcp output, format the message before sending.
 
   config_name "tcp_custom"
   milestone 1
 
-  config :mapping, :validate => :hash  
+  # Output only the specified fields or the entire event by default.
+  #
+  #     mapping => {"server" => "%{machine}"}
+  config :mapping, :validate => :hash
+
+  # Convert values using `scanf` where needed.
+  #
+  #     convert => {"port" => "%d"}
+  config :convert, :validate => :hash
   
   public
   def receive(event)
@@ -19,6 +31,12 @@ class LogStash::Outputs::TcpCustom < LogStash::Outputs::Tcp
       end
     else
       evt = event.to_hash
+    end
+
+    if @convert
+      @convert.each do |k,f|
+        evt[k] = evt[k].scanf(f)[0]
+      end
     end
 
     @codec.encode(evt)
